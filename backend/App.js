69d678645ea5256b1f06d7976cc467e72e4006ae
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-08-01 21:48:16 +0300

    Created Url For Products

--- backend/App.js
+++ backend/App.js
@@ -5,5 +5,14 @@ const cookieParsel = require("cookie-parser")
 app.use(express.json())
 app.use(cookieParsel())
 
+const products = require("./Routes/productRoute")
+
+app.use("/api/v1", products)
+
+
+const errorMiddleware = require("./Middleware/Error")
+
+app.use(errorMiddleware)
+
 
 module.exports = app
