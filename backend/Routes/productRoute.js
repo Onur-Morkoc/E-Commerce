13cec01a08bd27c96e50832fd6d1d25679b4893b
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-08-02 21:10:09 +0300

    created authorization processes on router for products

--- backend/Routes/productRoute.js
+++ backend/Routes/productRoute.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const { getAllProducts, createProduct, UpdateProduct, deleteProduct, getProductDetails } = require("../Controllers/productController")
+const { isAuthenticatedUser, authorizeRoles } = require("../Middleware/auth")
 
 const router = express.Router()
 
-router.route("/products").get(getAllProducts)
+router.route("/products").get(isAuthenticatedUser, authorizeRoles("Admin"), getAllProducts)
 
-router.route("/product/new").post(createProduct)
+router.route("/product/new").post(isAuthenticatedUser, authorizeRoles("Admin"), createProduct)
 
-router.route("/product/:id").put(UpdateProduct).delete(deleteProduct).get(getProductDetails)
+router.route("/product/:id")
+    .put(isAuthenticatedUser, authorizeRoles("Admin"), UpdateProduct)
+    .delete(isAuthenticatedUser, authorizeRoles("Admin"), deleteProduct)
+    .get(getProductDetails)
 
 module.exports = router
