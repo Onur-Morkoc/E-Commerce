7a2c780bc12e436d7f30b4052f8cd52c64aa8fbf
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-08-05 02:35:53 +0300

    Created function for Delete Product Review

--- backend/Controllers/productController.js
+++ backend/Controllers/productController.js
@@ -133,7 +133,6 @@ exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
 
 })
 
-
 // All Product Reviews
 exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
 
@@ -144,6 +143,42 @@ exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
     res.status(200).json({
         success: true,
         reviews: product.reviews
+    })
+
+})
+
+// Delete Product Review
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+
+    const product = await Product.findById(req.query.productId)
+
+    if (!product) return next(new ErrorHander("Product Not Found", 404))
+
+    const reviews = product.reviews.filter(rev => rev._id.toString() !== req.query.id.toString())
+
+    let avg = 0
+
+    reviews.forEach(rev => avg += rev.rating)
+
+    const ratings = avg / reviews.length
+
+    const numOfReviews = reviews.length
+
+    await Product.findByIdAndUpdate(
+        req.query.productId,
+        {
+            reviews,
+            ratings,
+            numOfReviews
+        },
+        {
+            new: true,
+            runValidators: true
+        })
+
+
+    res.status(200).json({
+        success: true
     })
 
 })
