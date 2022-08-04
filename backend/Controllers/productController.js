3475c795f2fe10a9b0473d61f6eded75b9a435fa
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-08-05 02:34:41 +0300

    Created function for All Product Reviews

--- backend/Controllers/productController.js
+++ backend/Controllers/productController.js
@@ -132,3 +132,18 @@ exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
     })
 
 })
+
+
+// All Product Reviews
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+
+    const product = await Product.findById(req.query.id)
+
+    if (!product) return next(new ErrorHander("Product Not Found", 404))
+
+    res.status(200).json({
+        success: true,
+        reviews: product.reviews
+    })
+
+})
