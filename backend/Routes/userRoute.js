f06d9e239af17dd67124ce4db7d70facd6186a10
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-08-05 02:42:48 +0300

    New route created for user

--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require("express")
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require("../Controllers/userController")
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile, getAllUsers, getSingleUser, deleteUser, updateUserRole } = require("../Controllers/userController")
+const { isAuthenticatedUser, authorizeRoles } = require("../Middleware/auth")
 
 const router = express.Router()
 
@@ -7,12 +8,23 @@ router.route("/register").post(registerUser)
 
 router.route("/login").post(loginUser)
 
-router.route("/logout").post(logout)
+router.route("/logout").get(logout)
 
 router.route("/password/forgot").post(forgotPassword)
 
 router.route("/password/reset/:token").post(resetPassword)
 
+router.route("/me").get(isAuthenticatedUser, getUserDetails)
 
+router.route("/password/update").put(isAuthenticatedUser, updatePassword)
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile)
+
+router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles("Admin"), getAllUsers)
+
+router.route("/admin/user/:id")
+.get(isAuthenticatedUser, authorizeRoles("Admin"), getSingleUser)
+.put(isAuthenticatedUser, authorizeRoles("Admin"), updateUserRole)
+.delete(isAuthenticatedUser, authorizeRoles("Admin"), deleteUser)
 
 module.exports = router
