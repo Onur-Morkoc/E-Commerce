5a33b1b6df42041783e2f2bc542f57ce47803047
Onur-Morkoc <onurmorkoc00@gmail.com>
2022-08-05 18:44:00 +0300

    Created function for Update Order Status, Delete Order

--- backend/Controllers/orderController.js
+++ backend/Controllers/orderController.js
@@ -57,18 +57,63 @@ exports.myOrder = catchAsyncErrors(async (req, res, next) => {
 
 })
 
-// All Orders --Admin
+// All Orders -- Admin
 exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
-    const orders = await Order.find({ })
+    const orders = await Order.find({})
 
     let totalAmount = 0
 
-    orders.forEach(order=>totalAmount+=order.totalPrice)
+    orders.forEach(order => totalAmount += order.totalPrice)
 
     res.status(200).json({
         success: true,
         totalAmount,
         orders
+    })
+
+})
+
+// Update Order Status -- Admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id)
+
+    if (!order) return next(new ErrorHander(`Order Not Found With This Id: ${req.params.id}`, 500))
+
+    if (order.orderStatus == "Delivered") return next(new ErrorHander("You Have Already Delivered This Order", 400))
+
+    order.orderItems.forEach(async o => await updateStock(o.product, o.quantity))
+
+    order.orderStatus= req.body.status
+
+    if (req.body.status == "Delivered") order.deliveredAt = Date.now()
+
+    await order.save({ validateBeforeSave: false })
+
+    res.status(200).json({
+        success: true
+    })
+
+})
+
+async function updateStock(id, quantity) {
+    const product = await Product.findById(id)
+
+    product.stock -= quantity
+
+    await product.save({ validateBeforeSave: false })
+
+}
+
+// Delete Order -- Admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id)
+
+    if (!order) return next(new ErrorHander(`Order Not Found With This Id: ${req.params.id}`, 500))
+
+    await order.remove()
+
+    res.status(200).json({
+        success: true
     })
 
 })
